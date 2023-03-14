54603899af2749ff2cdc66463c45bb004dfcdeec
Kolawole Moses <xoloyinks@gmail.com>
2023-03-14 02:23:16 -0700

    Date added

--- src/weather.js
+++ src/weather.js
@@ -11,18 +11,17 @@ export default function Weather() {
     const [icon , setIcon] = useState([]);
     const [devCity, setDevcity] = useState("");
     const [city , setCity] = useState("Lagos");
-    const [devCountry, setDevcountry] = useState("");
-    const [country, setCountry] = useState("Nigeria");
 
     useEffect(() => {
-        fetch(`https://api.weatherapi.com/v1/current.json?key=c379f3114aa5430bbc7165817231303&q=${city} ${country}&aqi=no`)
+        if(city === "") return;
+        fetch(`https://api.weatherapi.com/v1/current.json?key=c379f3114aa5430bbc7165817231303&q=${city}&aqi=no`)
         .then(res => res.json())
         .then((data) => {
             setLocation(data.location);
             setCurrent(data.current);
             setIcon(data.current.condition)
     })
-    } ,[city, country])
+    } ,[city])
 
     const forcast = (e) => {
         
@@ -37,27 +36,31 @@ export default function Weather() {
                 </div>
                 <br />
                 <div className='px-3 py-4 flex flex-col gap-3'>
-                    <p><input type="text" onChange={(e) => setDevcity(e.target.value)} value={devCity} placeholder='Enter your city' className='input p-3 text-sm text-black font-bold w-full rounded' /></p>
-                    <p><input type="text" onChange={(e) => setDevcountry(e.target.value)} value={devCountry} placeholder='Enter your country' className='input p-3 text-sm text-black font-bold w-full rounded' /></p>
-                    <button onClick={() => {setCity(devCity); setCountry(devCountry)}} className='px-8 py-4 w-[fit-content] mx-auto bg-gray-900 rounded-full text-sm'>Check</button>
+                    <p><input type="text" onChange={(e) => setDevcity(e.target.value)} value={devCity} placeholder='Enter location here...' className='input p-3 text-sm text-black font-bold w-full rounded' /></p>
+                    <button onClick={() => {setCity(devCity)}} className='px-8 py-4 w-[fit-content] mx-auto bg-transparent border-2 border-white rounded-full text-sm hover:bg-white hover:text-black'>Check</button>
                 </div>
-                <div className='w-10/12 h-[40%] mx-auto rounded-[20px] information '>
+                <div className='w-10/12 h-[auto] mx-auto rounded-[20px] information py-4 '>
 
                     <div className='text-black font-bold text-2xl text-center py-3'>
-                        {location.name}, {location.country}
+                        {location.name} <br /> {location.country}
                     </div>
                     <div className='icon flex items-center justify-center'>
                         <span><img src={icon.icon} alt="" /></span>
                         
                     </div>
+                    <div className='text-center'>
+                        <span className='font-semibold text-sm  '>{icon.text}</span>
+                    </div>
                     <div className='font-bold flex items-center text-3xl p-3 rounded-[20px] w-[fit-content] mx-auto'>
                         <span><BsThermometerHalf color='red' className='mx-auto mb-5 text-5xl' /> </span>
                         <span className='border-r-2 border-gray pr-3 mr-3'>{current.temp_c}&#8451;</span>
                         <span>{current.temp_f}&#8457;</span>
+                        
                     </div>
-                    {/* <div className='text-center'>
-                        <span className='font-semibold text-3xl  '>{current.condition.text}</span>
-                    </div> */}
+                    <div className='text-sm text-center'>
+                    <span>{location.localtime}</span>
+                    </div>
+                    
                 </div>
             </div>
         </section>
