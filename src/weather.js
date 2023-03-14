ed708820a6eebbb3d69d458a6bdaa05a186fb3f6
Kolawole Moses <xoloyinks@gmail.com>
2023-03-14 01:30:56 -0700

    Custom search added

--- src/weather.js
+++ src/weather.js
@@ -9,27 +9,26 @@ export default function Weather() {
     const [location , setLocation] = useState([]);
     const [current , setCurrent] = useState([]);
     const [icon , setIcon] = useState([]);
+    const [devCity, setDevcity] = useState("");
+    const [city , setCity] = useState("Lagos");
+    const [devCountry, setDevcountry] = useState("");
+    const [country, setCountry] = useState("Nigeria");
 
     useEffect(() => {
-        fetch("https://api.weatherapi.com/v1/current.json?key=c379f3114aa5430bbc7165817231303&q=Canada&aqi=no")
+        fetch(`https://api.weatherapi.com/v1/current.json?key=c379f3114aa5430bbc7165817231303&q=${city} ${country}&aqi=no`)
         .then(res => res.json())
         .then((data) => {
             setLocation(data.location);
             setCurrent(data.current);
             setIcon(data.current.condition)
     })
-    } ,[])
+    } ,[city, country])
+
+    const forcast = (e) => {
+        
+    }
   return (
     <>
-        {/* <div className='cloud_1 z-20 absolute'>
-            <img src={cloud_1} alt="" className='w-[200px] h-[200px]' />
-        </div>
-        <div className='cloud_2 z-10 absolute'>
-            <img src={cloud_2} alt="" className='w-[50px] h-[50px] drop-shadow-lg ' />
-        </div>
-        <div className='cloud_3 z-30 absolute'>
-            <img src={cloud_3} alt="" className='w-[50px] h-[50px] drop-shadow-lg ' />
-        </div> */}
         <section className='relative z-50 w-screen h-screen'>
             <div className='section absolute top-0 bottom-0 left-0 right-0'></div>
             <div className='absolute top-0 bottom-0 left-0 right-0 backdrop-blur-lg text-xl text-white bg-white/3'>
@@ -37,12 +36,17 @@ export default function Weather() {
                     Cloud x
                 </div>
                 <br />
+                <div className='px-3 py-4 flex flex-col gap-3'>
+                    <p><input type="text" onChange={(e) => setDevcity(e.target.value)} value={devCity} placeholder='Enter your city' className='input p-3 text-sm text-black font-bold w-full rounded' /></p>
+                    <p><input type="text" onChange={(e) => setDevcountry(e.target.value)} value={devCountry} placeholder='Enter your country' className='input p-3 text-sm text-black font-bold w-full rounded' /></p>
+                    <button onClick={() => {setCity(devCity); setCountry(devCountry)}} className='px-8 py-4 w-[fit-content] mx-auto bg-gray-900 rounded-full text-sm'>Check</button>
+                </div>
                 <div className='w-10/12 h-[40%] mx-auto rounded-[20px] information '>
 
                     <div className='text-black font-bold text-2xl text-center py-3'>
                         {location.name}, {location.country}
                     </div>
-                    <div className='flex items-center justify-center'>
+                    <div className='icon flex items-center justify-center'>
                         <span><img src={icon.icon} alt="" /></span>
                         
                     </div>
